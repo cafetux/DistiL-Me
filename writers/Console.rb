e3970c694dd9762280fb2978b8778a9b0428cb66
Fabien Maury <fab.maury@gmail.com>
2014-02-15 19:29:25 +0100

    add simple dummy console writer, and a twitter writer: for tweet content from file

---
+++ writers/Console.rb
@@ -0,0 +1,9 @@
+class Console
+  def initialize(config)
+
+  end
+  def send(text)
+    puts text
+  end
+
+end
---
+++ writers/TweetWriter.rb
@@ -0,0 +1,24 @@
+require 'twitter'
+require_relative '../utils/ConfigArgumentCheck'
+
+class TweetWriter
+  include ConfigArgumentCheck
+
+  def initialize(myConfig)
+    checkIsPresent(myConfig,:consumer_key)
+    checkIsPresent(myConfig,:consumer_secret)
+    checkIsPresent(myConfig,:access_token)
+    checkIsPresent(myConfig,:access_token_secret)
+
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = myConfig[:consumer_key]
+      config.consumer_secret     = myConfig[:consumer_secret]
+      config.access_token        = myConfig[:access_token]
+      config.access_token_secret = myConfig[:access_token_secret]
+    end
+  end
+  def send(text)
+    @client.update(text)
+  end
+
+end
