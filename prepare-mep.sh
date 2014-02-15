b478502704d2dcb45fb42c912ef40b5847b9440e
Fabien Maury <fab.maury@gmail.com>
2014-02-15 19:52:36 +0100

    script to move files into heroku directory (that contains real configuration)

---
+++ prepare-mep.sh
@@ -0,0 +1,8 @@
+production_rep=$1
+cp readers $production_rep -rf
+cp writers $production_rep -rf
+cp Main.rb $production_rep
+cp Job.rb $production_rep
+cp utils $production_rep -rf
+cp Gemfile $production_rep
+cp resources $production_rep -rf
